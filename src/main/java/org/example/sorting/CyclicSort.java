c777fc7d165eab38e67392c661aec0a0da4c5248
Nhat Anh <54491115+ZNhatAnhZ@users.noreply.github.com>
2023-04-27 15:18:52 +0700

    add some comment on cyclic sort

--- src/main/java/org/example/sorting/CyclicSort.java
+++ src/main/java/org/example/sorting/CyclicSort.java
@@ -9,19 +9,23 @@ public class CyclicSort {
             do {
                 position = i;
                 for(int j = i+1; j < array.length; j++) {
+                    //find the sorted position of the current element
                     if (currentElement > array[j]) {
                         position++;
                     }
                 }
 
+                //check to see if this is the same element
                 if (i == position && currentElement == array[position]) {
                     break;
                 }
 
+                //add duplicate element behind each other
                 while(currentElement == array[position]) {
                     position++;
                 }
 
+                //swapping elements with a var and a position inside the array
                 int temp = array[position];
                 array[position] = currentElement;
                 currentElement = temp;
